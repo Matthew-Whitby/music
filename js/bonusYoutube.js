cae123868e30b5e2446aac43bb59f945edfc91ee
Whitbyfish <matj1@aol.com>
2018-11-18 23:03:53 +0000

    Update bonusYoutube.js

--- js/bonusYoutube.js
+++ js/bonusYoutube.js
@@ -135,7 +135,7 @@ function GenerateHtml(){
 var c=[];
 //var c=["this song hasn't been worked on in ages","tried improving mixing on vocals, with no success","tried improving mixing on vocals, with some success","tried improving mixing on vocals, with some more success","That's right, it's being remade again","what can I say, it's my favourite song","also throwing in a lot more of my own additions to the song","why did I even try uploading it, I have barely started on it","who knows if this'll get finished. Maybe it'll... disappear","one of my previous favourite songs","possibly somewhat close to being finished, and has so for half a year","far from done","tried to do some 'pop' music","tried even harder","need to fix lower notes","just realised, the videos are going to be ordered backwards so read these upside down!"];
 let parent=document.getElementById("timeline");
-for(i=0;i<vidIdList.length;i++){
+for(i=vidTotal;i<vidIdList.length;i++){
    let upload=document.createElement("section");
    parent.appendChild(upload);
    upload.classList.add("card");
