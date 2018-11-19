304247175559926c10569ed57305a3919e77a760
Whitbyfish <matj1@aol.com>
2018-11-19 17:15:58 +0000

    a

--- js/youtube.js
+++ js/youtube.js
@@ -1,4 +1,5 @@
 var nextToken;
+var vsc=10;
 function GetYoutubeData(w,callback){
 	let c=document.getElementById("vid_"+w);
 	let v=c.getElementsByClassName("movie")[0].getAttribute("data-youtube");
@@ -10,15 +11,17 @@ function GetYoutubeData(w,callback){
 			id:v
 		},function(data){
 			let r=data.items[0].statistics.likeCount;
-			let p=data.items[0].snippet.thumbnails.maxres.url;
+			p="https://i.ytimg.com/vi/"+c.getElementsByClassName("movie")[0].getAttribute("data-youtube")+"/maxresdefault.jpg";
 			let t=data.items[0].snippet.title;
 			let v=data.items[0].statistics.viewCount;
 			v=WriteNumber(v);
 			if(r==null)r=53;
 			r=WriteNumber(r);
 			c.getElementsByClassName("likes-count")[0].innerHTML=r;
-			c.getElementsByTagName("img")[0].setAttribute("data-src",p);
-			c.getElementsByTagName("img")[0].setAttribute("src",p);
+			if(p!=null){
+				c.getElementsByTagName("img")[0].setAttribute("data-src",p);
+				c.getElementsByTagName("img")[0].setAttribute("src",p);
+			}
 			let d=data.items[0].snippet.publishedAt;
 			let x=d.substring(0,10);
 			let s=x.split('-');
@@ -61,18 +64,17 @@ function GetVidIds(){
 			key:"AIzaSyD6XBI5r8UWTPCtF00EwJOb5ZlxunvxYTw",
 			part:"snippet",
 			playlistId:"PL1BxM-1kDL2jDaZWiNUWk7_gOuM_m7MZr",
-			maxResults:"50"
+			maxResults:vsc
 		},function(data){
 			let count=data.pageInfo.totalResults;
 			varIds.push(count);
-			alert("Count = "+count);
-			for(i=0;i<count;i++)
-			{
+			for(i=0;i<count;i++){
 				document.getElementById("vid_"+(i+1)).getElementsByClassName("movie")[0].setAttribute("data-youtube",data.items[i].snippet.resourceId.videoId);
 			}
 		});
 	return vidIds;
 }
+
 function GetVidIdsUploads(callback){
 	$.get("https://www.googleapis.com/youtube/v3/channels",{
 			part: 'contentDetails',
@@ -87,8 +89,9 @@ function GetVidIdsUploads(callback){
 function getPlaylistVids(pid,callback){
 	$.get("https://www.googleapis.com/youtube/v3/playlistItems",{
 			part:'snippet',
-			maxResults:50,
+			maxResults:vsc,
 			playlistId:pid,
+			pageToken:nextToken,
 			key:'AIzaSyD6XBI5r8UWTPCtF00EwJOb5ZlxunvxYTw'
 		},function(data){
 			$.each(data.items,function(i,item){
@@ -102,19 +105,12 @@ function GenerateIds(){
 	let n=document.getElementsByClassName("vid_upload");
 	for(i=0;i<n.length;i++)n[i].id="vid_"+(i+1);
 }
-function GetNoVids(){
-	let v=1;
-	do{let g=document.getElementById("vid_"+v);
-		if(g!=null)v++;
-		else return v;
-	}while(true);
-}
 function GenerateHtml(){
 	//var v=["FnAmgwiHZtg","KRybbNpSHwU","POFLrlcnAdk","qDhF9L5zFnI","8JlW8kyoBgY","p-x5uukSQUo","MR2FZi92BMY","_mn4Jh3j_zk","eCwiCq71lCk","GV8tRmnfNRo","aRP9l_sGdbo","0RLMAoanOVE","5DD3CrZBLm4","SzL3_moFeWw","nX_lsrqgrpU","D68FyZixZ3M","zVtLwaVmVfI","PWSdtbl4DsM","Cyh0q0GfyCg","EW3ESox9A6M","SkyL8KUQNZ4","LYrHuZ3yzdw","5ZG5WcihXNg","Zn7rbfNBYwA","3Lu1bs2APDU"];
 	var c=["First Cover！Recorded with ancient technology.","First Sakura Gakuin cover, pretty easy stuff! Been aaaaaaages since the last cover.","A little acoustic cover, pretty bad timing though♪","A cheeky little piano cover of a pretty good, but underrated japanese band.","First Full instrumental cover!!","2 covers in one day, What's going on?!","My favourite cover to date♪","A bit more Walküre","Second piano cover, yay!","My favourite song from Mrs GREEN APPLE!","A cheeky little anime intro!","Piano cover of Momoiro Clover Z's latest song!","Even more Momoiro Clover Z","Vocaloid Singing hashire","Rolling Girl","Kinyoubi no Ohayou instrmental","Kinyoubi no Ohayou trumpet","Kinyoubi no Ohayou Gumi (meant to be sung by a male)","Kinyoubi no Ohayou Gumi Another Story","why did this get 2k views?","acoustic cover thing","probably the best mixed thing I have made thus far","thought the vocaloid sounded slightly like the original singer so made this","don't really know why I covered this song","fully instrumental fast song"];
 	c.reverse();
 	var parent=document.getElementById("timeline");
-	for(i=0;i<vidIdList.length;i++){
+	for(i=vidTotal;i<vidIdList.length;i++){
 		var upload=document.createElement("section");
 		parent.appendChild(upload);
 		upload.classList.add("card");
@@ -185,25 +181,28 @@ var vidsLoaded=0;
 var totalVids;
 function KeepOrder(e){
    totalVids=e;
-   for(i=1;i<e;i++)GetYoutubeData(i,()=>{vidsLoaded++;sortDates();});
+   for(i=1;i<(vsc+1);i++)GetYoutubeData(vidTotal+i,()=>{vidsLoaded++;sortDates();});
 }
 function sortDates(){
-   if(vidsLoaded==totalVids-1){
-      for(j=1;j<totalVids;j++){
-         c=document.getElementById("vid_"+j);
+   if(vidsLoaded==totalVids){
+      for(j=1;j<(vsc+1);j++){
+         c=document.getElementById("vid_"+(vidTotal+j));
          SortDate(c);
 		}
-		document.getElementById("topScroll").addEventListener("click",DateClicked);
-		l=document.createElement("li");
-		l.classList.add("scrollBtn");
-		l.id="bottomScroll";
-		a=document.createElement("a");
-      l.appendChild(a);
-      p=document.createElement("span");
-      a.appendChild(p);
-      p.innerHTML="Bottom";
-		document.getElementById("sidenavList").appendChild(l);
-		l.addEventListener("click",DateClicked);
+		vidTotal+=vsc;
+      if(document.getElementById("bottomScroll")==null){
+			document.getElementById("topScroll").addEventListener("click",DateClicked);
+			l=document.createElement("li");
+			l.classList.add("scrollBtn");
+			l.id="bottomScroll";
+			a=document.createElement("a");
+      	l.appendChild(a);
+      	p=document.createElement("span");
+      	a.appendChild(p);
+      	p.innerHTML="Bottom";
+			document.getElementById("sidenavList").appendChild(l);
+			l.addEventListener("click",DateClicked);
+		}
    }
 }
 function SortDate(c){
@@ -227,4 +226,14 @@ function SortDate(c){
       d.appendChild(c);
 		l.addEventListener("click",DateClicked);
    }else document.getElementById("y"+y).appendChild(c);
+}
+function GetNextVids(){
+   if(!loadingB){
+      loadingB=true;
+      GetVidIdsUploads(()=>{
+         GenerateHtml();
+         GenerateIds();
+         KeepOrder(document.getElementsByClassName("vid_upload").length);
+         loadingB=false;});
+   }
 }
